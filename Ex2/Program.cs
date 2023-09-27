2722eace01a17c31ffa90e14f7813aaed9b397f2
Kleo <mmttdd16@gmail.com>
2023-09-27 06:08:56 +0300

    FInished Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу замена элементов
+// массива:  положительные эллементы замените
+// на сответствующие отрицательные и наоборот.
+
+int WorkWithUser (string message)
+{
+    System.Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[] FillArray(int length, int min, int max)
+{
+    int[] result = new int[length];
+    Random rand = new Random(); // Create random object
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rand.Next(min, max + 1); // Использолвание одного и того же Random
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
+
+void Opposite(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        inArray[i] = inArray[i] * -1;
+    }
+}
+
+
+int size = WorkWithUser("Введите размер массива: ");
+int minValue = WorkWithUser("Введите минимальное значение: ");
+int maxValue = WorkWithUser("Введите максимальное значение: ");
+int[] array = FillArray(size, minValue, maxValue);
+PrintArray(array);
+System.Console.WriteLine();
+Opposite(array);
+PrintArray(array);
+
